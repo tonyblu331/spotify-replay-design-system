11d37eb3c23ec3fdf5d3c177d5cab2db486d34d7
juancs <juanchozass@gmail.com>
2022-12-10 14:11:50 +0100

    prettier config update

--- src/components/sr-list/sr-list.stories.ts
+++ src/components/sr-list/sr-list.stories.ts
@@ -5,7 +5,8 @@ export default {
       description: 'Items to be injected in the component slot',
     },
     ordered: {
-      description: 'Dictates whether to use an ordered list instead of an unordered list.',
+      description:
+        'Dictates whether to use an ordered list instead of an unordered list.',
       defaultValue: false,
       control: {
         type: 'boolean',
@@ -21,7 +22,18 @@ const Template = ({ items }) => `
 `;
 
 export const unordered = Template.bind({});
-unordered.args = { items: ['<sr-list-item>List item</sr-list-item>', '<sr-list-item>Another list item</sr-list-item>'] };
+unordered.args = {
+  items: [
+    '<sr-list-item>List item</sr-list-item>',
+    '<sr-list-item>Another list item</sr-list-item>',
+  ],
+};
 
 export const ordered = Template.bind({});
-ordered.args = { items: ['<sr-list-item>Item 1</sr-list-item>', '<sr-list-item>Item 2</sr-list-item>'], ordered: true };
+ordered.args = {
+  items: [
+    '<sr-list-item>Item 1</sr-list-item>',
+    '<sr-list-item>Item 2</sr-list-item>',
+  ],
+  ordered: true,
+};
