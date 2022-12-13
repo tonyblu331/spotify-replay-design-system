f0626e8f4b9337b6ef7cd887fc34aff65fe6bcbf
juancs <juanchozass@gmail.com>
2022-12-13 19:21:37 +0100

    combobox skeleton

--- src/components/form/sr-combobox/sr-combobox.stories.ts
+++ src/components/form/sr-combobox/sr-combobox.stories.ts
@@ -1,23 +1,38 @@
 export default {
   title: 'Components/Form/Combo box',
-  // TODOJCS
-  // argTypes: {
-  //   variant: {
-  //     options: ['neutral', 'error'], // Value presets
-  //     control: { type: 'select' }, // Knob type. It can be select, radio, etc
-  //     description: 'The button variant', // The description text displayed in the knobs table
-  //   },
-  // },
+  argTypes: {
+    options: {
+      description: 'The available options selectable from to the combobox',
+    },
+    labelText: {
+      control: 'text',
+      description:
+        'The text to add to the label that is paired with the combo box',
+    },
+    allowEmpty: {
+      control: 'boolean',
+      description:
+        'If true, the checkbox will allow the selection of an empty value',
+    },
+  },
 };
 
-const Template = args => `
-  <sr-combobox>
-    ${args.content}
-  </sr-combobox>
-`;
+const Template = ({ labelText, options, allowEmpty }) =>
+  `<sr-combobox 
+      ${labelText ? `labelText="${labelText}"` : ''} 
+      ${allowEmpty ? 'allowEmpty' : ''}>
+    ${options}
+  <sr-combobox/>`;
 
-export const ComponentStory = Template.bind({});
-ComponentStory.args = {
-  // Populate this object with key/value pairs, customizing the component atribute values or even its content. Eg:
-  // content: 'Demo text',
+export const Labelless = Template.bind({});
+Labelless.args = {
+  options: `<option>Potáto</option><option>Potäto</option>`,
+  allowEmpty: false,
+};
+
+export const Labelled = Template.bind({});
+Labelled.args = {
+  options: `<option>Avatar</option><option>Ghost in the Shell</option><option>Memento</option>`,
+  labelText: 'My favorite movie',
+  allowEmpty: false,
 };
